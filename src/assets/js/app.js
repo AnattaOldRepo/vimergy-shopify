64ec72489129f194b052c2c52a6f8deb7b222e62
maverickmj <maninder@anattadesign.com>
2018-05-15 19:22:54 +0530

    home page development

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -7,15 +7,15 @@ $('.js-modal').modal();
 
 // $('.lazy-load').Lazy();
 
-$('.js-testimonals-carousel').slick({
-    prevArrow: '<button type="button" class="slick-prev slick-arrow" style="display: block;"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 71.831 71.831" enable-background="new 0 0 71.831 71.831" xml:space="preserve"><g><rect x="16.33" y="45.952" transform="matrix(0.7071 0.7071 -0.7071 0.7071 44.937 -10.5836)" width="37.828" height="6"></rect><rect x="16.331" y="23.202" transform="matrix(-0.7071 0.7071 -0.7071 -0.7071 78.6933 19.8089)" width="37.826" height="6"></rect></g></svg></button>',
-    nextArrow: '<button type="button" class="slick-next slick-arrow" style="display: block;"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 71.831 71.831" enable-background="new 0 0 71.831 71.831" xml:space="preserve"><g><rect x="16.33" y="45.121" transform="matrix(-0.7071 0.7071 -0.7071 -0.7071 94.1925 57.2278)" width="37.828" height="6"></rect><rect x="16.331" y="22.371" transform="matrix(0.7071 0.7071 -0.7071 0.7071 28.2625 -17.4903)" width="37.826" height="6"></rect></g></svg></button>',
+$('.js-products-carousel').slick({
+    prevArrow: '<svg class="slick-prev slick-arrow" style="display: block;" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 71.831 71.831" enable-background="new 0 0 71.831 71.831" xml:space="preserve"><g><rect x="16.33" y="45.952" transform="matrix(0.7071 0.7071 -0.7071 0.7071 44.937 -10.5836)" width="37.828" height="6"></rect><rect x="16.331" y="23.202" transform="matrix(-0.7071 0.7071 -0.7071 -0.7071 78.6933 19.8089)" width="37.826" height="6"></rect></g></svg>',
+    nextArrow: '<svg class="slick-next slick-arrow" style="display: block;" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 71.831 71.831" enable-background="new 0 0 71.831 71.831" xml:space="preserve"><g><rect x="16.33" y="45.121" transform="matrix(-0.7071 0.7071 -0.7071 -0.7071 94.1925 57.2278)" width="37.828" height="6"></rect><rect x="16.331" y="22.371" transform="matrix(0.7071 0.7071 -0.7071 0.7071 28.2625 -17.4903)" width="37.826" height="6"></rect></g></svg>',
 
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
+    infinite: false,
+    slidesToShow: 4,
+    slidesToScroll: 4,
     responsive: [{
-            breakpoint: 1024,
+            breakpoint: 1700,
             settings: {
                 slidesToShow: 3,
                 slidesToScroll: 3,
@@ -33,11 +33,20 @@ $('.js-testimonals-carousel').slick({
         {
             breakpoint: 584,
             settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
+                slidesToShow: 2,
+                slidesToScroll: 2
             }
         }
     ]
+});
+$('.js-testimonals-carousel').slick({
+    prevArrow: '<a href="/" class="slick-prev--testimonial slick-arrow" style="display: block;">PREVIOUS</a>',
+    nextArrow: '<a href="/" class="slick-next--testimonial slick-arrow" style="display: block;">NEXT</a>',
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+
 });
 
 $('.c-mobileNav--expand').click(function(e) {
